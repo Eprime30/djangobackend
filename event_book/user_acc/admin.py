076c9d2773e3614d3f74e6679c2477ef57b8bf34
Eben <68605403+Eprime30@users.noreply.github.com>
2020-09-06 14:10:04 +0000

    first commit on django backend api

---
+++ event_book/user_acc/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from user_acc.models import Account
+# Register your models here.
+
+
+class AccountAdmin(UserAdmin):
+    list_display = ('email', 'username', 'first_name',
+                    'last_name', 'address', 'phone', 'city', 'is_admin', 'is_staff')
+    search_fields = ('email', 'username', 'last_name',)
+    readonly_fields = ('date_joined', 'last_login')
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+admin.site.register(Account, AccountAdmin)
